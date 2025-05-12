9e09104a0c0e4cb6ba0d4bcf19f235ab49ab14fd
Lilian-exe <lilianmura123910@gmail.com>
2025-05-12 11:54:49 +0300

    ASDd

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("registerForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Cont creat cu succes!");
+  });
+  
+  document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Autentificare reușită!");
+  });
